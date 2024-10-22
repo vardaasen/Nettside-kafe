6ae7911e92a85a052b2c7cd6b869675458cceac2
vardaasen <vardaasen@users.noreply.github.com>
2024-10-22 23:19:44 +0000

    deploy: bbe45653ecfefe9944c307f184fa1f80fd33e405

--- version.js
+++ version.js
@@ -1,2 +1,7 @@
-const appVersion = "2024.10.22.2122";
+<<<<<<< HEAD
+const appVersion = "2024.10.22.2108";
+window.appVersion = appVersion;
+=======
+const appVersion = "2024.10.22.2130";
+>>>>>>> main
 console.log(`App versjon: ${appVersion}`);
