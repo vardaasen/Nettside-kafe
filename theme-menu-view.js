530b9ddd1d69a1c20d395f5e90125da7f221be8d
vardaasen <vardaasen@users.noreply.github.com>
2024-10-23 07:12:59 +0000

    deploy: fbd8e1f39b31d9f4248bb8f240f70cd25141f105

--- theme-menu-view.js
+++ theme-menu-view.js
@@ -21,7 +21,7 @@ function createThemesHtml() {
       `;
 }
 
-function createThemeCardHtml(theme) {
+function createThemeCardHtmlOld(theme) {
   return /* HTML*/ `
         <div class="product-card">
           <div class='card-header-image' onclick='openThemeInfo()'>
@@ -30,6 +30,28 @@ function createThemeCardHtml(theme) {
           </div>
         </div>
       `;
+}
+
+function createThemeCardHtml(theme, price = 'kr') {
+  return /* HTML*/ `
+    <article class="product-card">
+      <header class="product-card__header" onclick="openThemeInfo()">
+        <img class="product-card__image" src="./img/themes/${theme}.webp" alt="${theme}">
+        <h2 class="product-card__title">${theme}</h2>
+      </header>
+      <footer class="product-card__footer">
+        <div class="product-card__price">${price}</div>
+        <button class="product-card__button-add button__add-to-cart" onclick="addThemeToCart('${theme}');">
+          <svg class="cart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <circle cx="9" cy="21" r="1"></circle>
+            <circle cx="20" cy="21" r="1"></circle>
+            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+          </svg>
+          Legg til
+        </button>
+      </footer>
+    </article>
+  `;
 }
 
 function openThemeInfo(theme) {}
@@ -45,7 +67,7 @@ function createThemeInfoOverlayElement() {
             <label for='product-comment'>Tilpasninger:</label>
             <textarea id='product-comment' oninput='updateComment(this.value)'></textarea>
           </div>
-    
+
           <div id='overlay-buy-row'>
             <div id='quantity-row'>
               <label for='quantity-input'>Antall:</label>
