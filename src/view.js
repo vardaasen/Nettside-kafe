2c5505bc7bc31693520f9f05540134dd612f6be9
vardaasen <git@vardaasen.no>
2024-10-23 15:30:02 +0200

    jobber med handlekurven

--- src/view.js
+++ src/view.js
@@ -1,22 +1,33 @@
 function updateView() {
-  document.getElementById('app').replaceChildren(createCurrentPageHtml());
+  const app = document.getElementById('app');
+  const content = createCurrentPageHtml();
+  
+  // Hvis content er et HTML element
+  if (content instanceof HTMLElement) {
+      app.replaceChildren(content);
+  } else {
+      // Hvis content er en string, lag et div element
+      const container = document.createElement('div');
+      container.innerHTML = content;
+      app.replaceChildren(container);
+  }
 }
 
 function createCurrentPageHtml() {
   if (model.app.currentPage === 'cafeMenu') {
-    return createCafeMenuHtml();
+      return createCafeMenuHtml();
   }
   if (model.app.currentPage === 'cakeThemes') {
-    return createThemesMenuHtml();
+      return createThemesMenuHtml();
   }
   if (model.app.currentPage === 'cakes') {
-    return createCakeMenuHtml();
+      return createCakeMenuHtml();
   }
   if (model.app.currentPage === 'shoppingCart') {
-    if (model.inputs.shoppingCart.case !== 'Overview') {
-        return createCheckoutView();
-    }
-    return createShoppingCartHtml();
+      if (model.inputs.shoppingCart.case !== 'Overview') {
+          return createCheckoutView();
+      }
+      return createShoppingCartHtml();
   }
   return '';
 }
