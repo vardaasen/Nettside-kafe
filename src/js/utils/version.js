7158019ca4610a99851913f05423f056b5aead4a
aleanon <104034507+aleanon@users.noreply.github.com>
2024-10-24 20:13:41 +0200

    Fjernet to duplikater etter feilet merging og endret fra dobbel fnutter til enkelt fnutter i Stringen for a blitgjøre eslint

--- src/js/utils/version.js
+++ src/js/utils/version.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-const appVersion = "2024.10.24.1355";
-=======
-const appVersion = "2024.10.24.1355";
->>>>>>> upstream/main
-=======
-const appVersion = "2024.10.24.1355";
->>>>>>> upstream/main
+const appVersion = '2024.10.24.1355';
+
 window.appVersion = appVersion;
 console.log(`App versjon: ${appVersion}`);
