434f6818688b583adae3189cfe916c1e8dbc7b04
Willads Storm-Ritter <vardaasen@users.noreply.github.com>
2024-10-25 11:58:32 +0200

    Update version.js

--- src/js/utils/version.js
+++ src/js/utils/version.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 const appVersion = "2024.10.25.0950";
-=======
-const appVersion = "2024.10.25.0950";
->>>>>>> upstream/main
-=======
-const appVersion = "2024.10.25.0950";
->>>>>>> upstream/main
 window.appVersion = appVersion;
 console.log(`App versjon: ${appVersion}`);
